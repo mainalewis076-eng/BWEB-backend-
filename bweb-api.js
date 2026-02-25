d5d89aef241ea3c7d15e83ab5e12e987bf0dc8c9
mainalewis076-eng <mainalewis076@gmail.com>
2026-02-25 15:23:18 +0300

    Add usage instructions to bweb-api.js
    Added instructions for including bweb-api.js in HTML pages.

--- bweb-api.js
+++ bweb-api.js
@@ -1,3 +1,7 @@
 // bweb-api.js
 // ============================================================
 // Add this file to your website folder
+// Include it in ALL your HTML pages like this:
+// <script src="bweb-api.js"></script>
+// ============================================================
+
